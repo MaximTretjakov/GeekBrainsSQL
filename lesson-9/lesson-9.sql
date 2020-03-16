3e436dc0e79fa074f636a203c9a34e7b244576a3
Maxim <victor-kovalov@yandex.ru>
2020-03-16 20:27:42 +0300

    Add lesson-9

--- lesson-9/lesson-9.sql
+++ lesson-9/lesson-9.sql
@@ -78,7 +78,23 @@ CALL HELLO(2)//
 */
 
 
+DELIMITER //
+DROP TRIGGER IF EXISTS check_products_insert//
+CREATE TRIGGER check_products_insert BEFORE INSERT ON products
+FOR EACH ROW
+BEGIN
+  SET NEW.name = COALESCE(NEW.name, 'default_name');
+  SET NEW.description = COALESCE(NEW.description, 'default_name');
+END//
 
+DELIMITER //
+DROP TRIGGER IF EXISTS check_products_update//
+CREATE TRIGGER check_products_update BEFORE UPDATE ON products
+FOR EACH ROW
+BEGIN
+  SET NEW.name = COALESCE(NEW.name, OLD.name, 'default_name');
+  SET NEW.description = COALESCE(NEW.description, OLD.description, 'default_name');
+END//
 
 
 
